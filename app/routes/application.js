efcba85e5e17d7092553ac505bf63b11e1eeda1d
Baptiste Meurant <baptiste.meurant@gmail.com>
2015-10-29 14:30:30 +0100

    Templates - 3 - Helpers

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  model: function() {
+  model: function () {
     // WARN : SOULD NOT BE DONE : We should not affect anything to windows but
-    // for the exercice, we want to access to comic from console today
-    window.comic = {title: "BlackSad"};
+    // for the exercice, we want to access to comics from console today
+    window.comics = [{title: "BlackSad"}, {title: "Calvin and Hobbes", scriptwriter: "Bill Watterson"}];
 
-    return window.comic;
+    return window.comics;
   }
 });
