42c900f0ebd49ccda8e3ddd1a18987ebb3925102
Baptiste Meurant <baptiste.meurant@gmail.com>
2015-11-05 23:04:02 +0100

    Add acceptance tests - Routing
    Conflicts:
    	tests/acceptance/01-templates-test.js

---
+++ tests/acceptance/02-routing-test.js
@@ -0,0 +1,209 @@
+import Ember from "ember";
+import { module, test } from 'qunit';
+import startApp from '../helpers/start-app';
+import comicsRoute from 'ember-training/routes/comics';
+
+let application;
+
+let Comic = Ember.Object.extend({
+  slug: '',
+  title: '',
+  scriptwriter: '',
+  illustrator: '',
+  publisher: ''
+});
+
+let blackSad = Comic.create({
+  slug: 'blacksad',
+  title: 'Blacksad',
+  scriptwriter: 'Juan Diaz Canales',
+  illustrator: 'Juanjo Guarnido',
+  publisher: 'Dargaud'
+});
+
+let calvinAndHobbes = Comic.create({
+  slug: 'calvin-and-hobbes',
+  title: 'Calvin and Hobbes',
+  scriptwriter: 'Bill Watterson',
+  illustrator: 'Bill Watterson',
+  publisher: 'Andrews McMeel Publishing'
+});
+
+let akira = Comic.create({
+  slug: 'akira',
+  title: 'Akira',
+  scriptwriter: 'Katsuhiro Otomo',
+  illustrator: 'Katsuhiro Otomo',
+  publisher: 'Epic Comics'
+});
+
+const COMICS = [akira, blackSad, calvinAndHobbes];
+
+let setupApp = function () {
+  comicsRoute.reopen({
+    model: function () {
+      return COMICS;
+    }
+  });
+  application = startApp();
+};
+
+module('01 - Routing Acceptance Tests', {
+  beforeEach() {
+    setupApp();
+  },
+  afterEach() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test("02 - Routing - 01 - Should display second level title", function (assert) {
+  assert.expect(4);
+
+  visit('/comics').then(function () {
+    assert.equal(find('.container').length, 1, "Page contains a container");
+
+    let $pageHeader = find('.container .page-header');
+    assert.equal($pageHeader.length, 1, "Page contains a page header");
+
+    let $title = find(".container .comics h2");
+    assert.equal($title.length, 1, "Page contains a h2 element");
+    assert.equal($title.text(), "Comics list", "Subtitle is correct");
+  });
+});
+
+test("02 - Routing - 02 - Should display text on comics/", function (assert) {
+  assert.expect(1);
+
+  visit('/comics').then(function () {
+    assert.ok(find("#no-selected-comic").text().indexOf("Please select on comic book for detailled information.") >= 0, "No selected comics text is displayed");
+  });
+});
+
+test("02 - Routing - 03 - Should display single comic zone", function (assert) {
+  assert.expect(1);
+
+  visit('/comics/akira').then(function () {
+    assert.equal(find(".selected-comic").length, 1, "Current selected comics zone is displayed");
+  });
+});
+
+test("02 - Routing - 04 - Should display the comic detail", function (assert) {
+  assert.expect(7);
+
+  visit('/comics/akira').then(function () {
+    let $selectedComic = find(".selected-comic");
+    assert.equal($selectedComic.length, 1, "Current selected comics zone is displayed");
+
+    let $title = $selectedComic.find("h3");
+    assert.equal($title.length, 1, "Comic title exists");
+    assert.ok($title.text().indexOf(akira.title) >= 0, "Comic title is correct");
+
+    let $props = $selectedComic.find("dl > dd");
+    assert.equal($props.length, 3, "Comic properties exist");
+    assert.ok($($props.get(0)).text().indexOf(akira.scriptwriter) >= 0, "Comic scriptwriter is correct");
+    assert.ok($($props.get(1)).text().indexOf(akira.illustrator) >= 0, "Comic illustrator is correct");
+    assert.ok($($props.get(2)).text().indexOf(akira.publisher) >= 0, "Comic publisher is correct");
+  });
+});
+
+test("02 - Routing - 05 - Should display links", function (assert) {
+  assert.expect(7);
+
+  visit('/comics/akira').then(function () {
+    let $comics = find(".comics ul > li > a");
+    assert.ok($comics.length >= 3, "Comics are displayed");
+
+    assert.ok($($comics.get(0)).attr('href').indexOf('/comics/' + akira.get('slug')) >= 0, "akira url is correct");
+    assert.ok($($comics.get(0)).hasClass('active'), "akira url is active");
+
+    assert.ok($($comics.get(1)).attr('href').indexOf('/comics/' + blackSad.get('slug')) >= 0, "blackSad url is correct");
+    assert.notOk($($comics.get(1)).hasClass('active'), "blackSad url is not active");
+
+    assert.ok($($comics.get(2)).attr('href').indexOf('/comics/' + calvinAndHobbes.get('slug')) >= 0, "calvinAndHobbes url is correct");
+    assert.notOk($($comics.get(2)).hasClass('active'), "calvinAndHobbes url is not active");
+  });
+});
+
+test("02 - Routing - 06 - Should display edit route", function (assert) {
+  assert.expect(7);
+
+  visit('/comics/akira/edit').then(function () {
+    let $selectedComic = find(".selected-comic");
+    assert.equal($selectedComic.length, 1, "Current selected comics zone is displayed");
+
+    let $form = $selectedComic.find("form");
+    assert.equal($form.length, 1, "Comic form exists");
+
+    let $props = $selectedComic.find("input");
+    assert.equal($props.length, 4, "Comic properties exist");
+    assert.ok($($props.get(0)).val().indexOf(akira.title) >= 0, "Comic title is correct");
+    assert.ok($($props.get(1)).val().indexOf(akira.scriptwriter) >= 0, "Comic scriptwriter is correct");
+    assert.ok($($props.get(2)).val().indexOf(akira.illustrator) >= 0, "Comic illustrator is correct");
+    assert.ok($($props.get(3)).val().indexOf(akira.publisher) >= 0, "Comic publisher is correct");
+  });
+});
+
+test("02 - Routing - 07 - Should link to edit route", function (assert) {
+  assert.expect(4);
+
+  visit('/comics/akira').then(function () {
+    let $selectedComic = find(".selected-comic");
+    assert.equal($selectedComic.length, 1, "Current selected comics zone is displayed");
+
+    let $btnEdit = $selectedComic.find(".btn-edit");
+    assert.equal($btnEdit.length, 1, "Edit button exists");
+
+    click($btnEdit).then(function () {
+      assert.equal(currentRouteName(), 'comic.edit', "Route name is correct");
+
+      let $form = find("form");
+      assert.equal($form.length, 1, "Comic form exists");
+    });
+  });
+});
+
+test("02 - Routing - 08 - Should display create route", function (assert) {
+  assert.expect(8);
+
+  visit('/comics/create').then(function () {
+    let $selectedComic = find(".selected-comic");
+    assert.equal($selectedComic.length, 1, "Current selected comics zone is displayed");
+
+    let $form = $selectedComic.find("form");
+    assert.equal($form.length, 1, "Comic form exists");
+
+    let $props = $selectedComic.find("input");
+    assert.equal($props.length, 4, "Comic properties exist");
+    assert.equal($($props.get(0)).val().length, 0, "Comic title is empty");
+    assert.equal($($props.get(1)).val().length, 0, "Comic scriptwriter is empty");
+    assert.equal($($props.get(2)).val().length, 0, "Comic illustrator is empty");
+    assert.equal($($props.get(3)).val().length, 0, "Comic publisher is empty");
+
+    let $comics = find(".comics ul > li");
+    assert.equal($comics.length, 4, "A new comic has been created");
+  });
+});
+
+test("02 - Routing - 09 - Should link to create route", function (assert) {
+  assert.expect(5);
+
+  visit('/comics').then(function () {
+    let $comics = find(".comics ul > li");
+    let comicsLength = $comics.length;
+    assert.ok(comicsLength >= 3, "Comics list displayed with more than 3 items");
+
+    let $addComic = find(".add-comic");
+    assert.equal($addComic.length, 1, "Create button exists");
+
+    click($addComic).then(function () {
+      assert.equal(currentRouteName(), 'comics.create', "Route name is correct");
+
+      let $form = find(".selected-comic form");
+      assert.equal($form.length, 1, "Comic form exists");
+
+      $comics = find(".comics ul > li");
+      assert.equal($comics.length, comicsLength + 1, "Comics list displayed with one supplementary item");
+    });
+  });
+});
