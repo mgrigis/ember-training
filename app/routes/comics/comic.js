821c361a676da330ad53c676170d50415df63dc6
Baptiste Meurant <baptiste.meurant@gmail.com>
2015-11-08 19:32:51 +0100

    Routing - 5 - LinkTo

--- app/routes/comics/comic.js
+++ app/routes/comics/comic.js
@@ -3,5 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model (params) {
     return this.modelFor('comics').findBy('slug', params.comic_slug);
+  },
+  serialize: function(model) {
+    return {
+      comic_slug: model.get('slug')
+    };
   }
 });
