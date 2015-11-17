4c4a151082a896f1ee91988b9f74e90e10725e14
Baptiste Meurant <baptiste.meurant@gmail.com>
2015-11-17 17:49:27 +0100

    Routing - 9 - renderTemplate

--- app/routes/comics/create.js
+++ app/routes/comics/create.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import Comic from 'ember-training/models/comic';
 
 export default Ember.Route.extend({
+  templateName: 'comic/edit',
+
   model () {
     let newComic = Comic.create({'slug': 'new'});
     this.modelFor('comics').pushObject(newComic);
