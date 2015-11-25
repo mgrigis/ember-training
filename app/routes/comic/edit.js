c342a39d06236611e9d56872bf8cfcb424b72b0e
Baptiste Meurant <baptiste.meurant@gmail.com>
2015-11-25 02:43:25 +0100

    Controllers - 3 - willTransition

--- app/routes/comic/edit.js
+++ app/routes/comic/edit.js
@@ -2,16 +2,36 @@ import Ember from 'ember';
 import Comic from 'ember-training/models/comic';
 
 export default Ember.Route.extend({
+
   afterModel (model) {
     this.set('initialModel', Comic.create(model));
   },
+
+  resetController (controller) {
+    controller.set('hasUserSavedOrCancel', false);
+  },
+
+  resetComic () {
+    this.get('controller.model').reset(this.get('initialModel'));
+  },
+
   actions: {
     save () {
       this.transitionTo('comic');
     },
     cancel () {
-      this.get('controller.model').reset(this.get('initialModel'));
+      this.resetComic();
       this.transitionTo('comic');
+    },
+    willTransition (transition) {
+      if (this.controller.get('hasUserSavedOrCancel')) {
+        return true;
+      } else if (confirm('Are you sure you want to abandon progress?')) {
+        this.resetComic();
+        return true;
+      } else {
+        transition.abort();
+      }
     }
   }
 });
